2e5da1419557cab0f3a726c0facc8f4cceb3c063
Trumpster <trumpster@oletters.com>
2025-12-03 03:59:38 -0330

    issues with buttons

--- scripts/questions.js
+++ scripts/questions.js
@@ -19,15 +19,56 @@ export async function loadQuestions() {
     // shuffle questions
     shuffle(questions);
 
+    // converts HTML entities &quot;, &amp into readable characters
+    function decodeHTML(str) {
+        const txt = document.createElement("textarea");
+        txt.innerHTML = str;
+        return txt.value;
+    }
+
+    shuffle(questions);
+
     // default number of questions
     let numOfQuestions = 10;
+    let formattedQuestions = [];
 
     for(let i = 0; i < numOfQuestions; i++){
-    let question = questions[i];
-    let question_options = shuffle([...question.incorrect_answers, question.correct_answer]);
+        let q = questions[i];
+        let decodedAnswers = [];
 
-    console.log(`Question ${i + 1} : ${question.question}\nOptions: ${question_options.join(", ")}\n`);
+        for (let i = 0; i < q.incorrect_answers.length; i++) {
+            decodedAnswers.push(decodeHTML(q.incorrect_answers[i]));
+        }
+
+        decodedAnswers.push(decodeHTML(q.correct_answer))
+
+
+
+        let shuffled = shuffle(decodedAnswers);
+
+        let options = {
+            A: shuffled[0],
+            B: shuffled[1],
+            C: shuffled[2],
+            D: shuffled[3]
+        };
+
+        let correctAnswerText = decodeHTML(q.correct_answer);
+        let answerKey = null;
+
+        for (let letter in options){
+            if(options[letter] === correctAnswerText){
+                answerKey = letter;
+                break;
+            }
+        }
+        formattedQuestions.push({
+            question: decodeHTML(q.question),
+            options: options,
+            answer: answerKey
+        })
   }
+  return formattedQuestions;
 }
 
-loadQuestions();
+// loadQuestions();
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // reference problem from scripts folder
-import { trivia } from "./trivia.js";
+// import { trivia } from "./trivia.js";
 
 import { loadQuestions } from "./questions.js";
 
@@ -30,21 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let timerInterval;
    
    
-
-    // Fisher Yates Shuffle Algorithm - ensure random questions each game
-    function getRandomQuestions() {
-
-        // create copy of trivia obj then suffle
-        const shuffled = [...trivia]
-
-        for (let i = shuffled.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-        }
-        return shuffled.slice(0, numberOfQuestions);
-    }
-
-    //  fixed bug
+     //  fixed bug
     function updateQuestionsLeft() {
       questionsLeft.textContent = numberOfQuestions - currentIndex;
     }
@@ -58,9 +44,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
         //need to reset timeLeft when next question loads
         timerInterval = setInterval(() => {
-            timeLeft-- 
+            timeLeft--;
 
-            // question timer
             if (timeLeft <= 0) {
                 clearInterval(timerInterval);
                 currentIndex++ // next question
@@ -76,8 +61,11 @@ document.addEventListener("DOMContentLoaded", () => {
         const buttons = [btnA, btnB, btnC, btnD];
 
         buttons.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                const userChoice = e.target.textContent;
+            button.addEventListener("click", () => {
+                
+                if (currentIndex >= currentQuestions.length) return;
+
+                const userChoice = this.textContent;
                 
                 const answerKey = currentQuestions[currentIndex].answer;
                 const correctAnswer = currentQuestions[currentIndex].options[answerKey];
@@ -96,20 +84,20 @@ document.addEventListener("DOMContentLoaded", () => {
         scoreDisplay.textContent = score;
     } 
 
-    function startQuiz() {
+    async function startQuiz() {
 
      // Reset
-        currentQuestions = getRandomQuestions();
+        currentQuestions = await loadQuestions();
         currentIndex = 0;
         score = 0;
 
         updateScoreDisplay();
 
-        // Load first question
+        
+        calculateScore();
         nextQuestion();
         updateQuestionsLeft();
         quizTimer();
-        calculateScore();
     }
 
     // need to fix next question function to loop through all questions
@@ -119,7 +107,7 @@ document.addEventListener("DOMContentLoaded", () => {
             question.textContent = `Quiz over! Final Score: ${score}/${numberOfQuestions}`
             return;
         }
-        // let question = currentQuestions[currentIndex]
+        
         
         const currentQuestion = currentQuestions[currentIndex];
 
