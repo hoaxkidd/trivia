f536182f24fa3871fa307ab571ac09810a5bacaa
Trumpster <trumpster@oletters.com>
2025-12-05 13:10:43 -0330

    form

---
+++ scripts/add_questions.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+   
+    const form = document.getElementById('edit-form');
+    const qInput = document.getElementById('question-input');
+    const aInput = document.getElementById('A-input');
+    const bInput = document.getElementById('B-input');
+    const cInput = document.getElementById('C-input');
+    const dInput = document.getElementById('D-input');
+    const correctSelect = document.getElementById('correct-option');
+    const addUpdateBtn = document.getElementById('add-update-btn');
+    const questionsContainer = document.getElementById('questions-container');
+    const tempCountDisplay = document.getElementById('temp-question-count');
+    const saveBtn = document.getElementById('save-questions-btn');
+
+    
+});
